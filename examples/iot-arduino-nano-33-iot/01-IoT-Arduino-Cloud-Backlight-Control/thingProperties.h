78e9b648af27fd26aca5ff6740303db56be250be
Marcin Saj <marcin@nixietester.com>
2021-10-25 23:05:06 +0200

    Update thingProperties.h

--- examples/iot-arduino-nano-33-iot/01-IoT-Arduino-Cloud-Backlight-Control/thingProperties.h
+++ examples/iot-arduino-nano-33-iot/01-IoT-Arduino-Cloud-Backlight-Control/thingProperties.h
@@ -7,9 +7,9 @@
 #include <ArduinoIoTCloud.h>
 #include <Arduino_ConnectionHandler.h>
 
-// **********************************************************
-const char THING_ID[] = "place-here-id-number-of-your-thing";
-// **********************************************************
+// *********************************************************************************
+const char THING_ID[] = "place-here-id-number-of-your-thing-from-iot-arduino-cloud";
+// *********************************************************************************
 
 const char SSID[]     = SECRET_SSID;    // Network SSID (name)
 const char PASS[]     = SECRET_PASS;    // Network password (use for WPA, or use as key for WEP)
