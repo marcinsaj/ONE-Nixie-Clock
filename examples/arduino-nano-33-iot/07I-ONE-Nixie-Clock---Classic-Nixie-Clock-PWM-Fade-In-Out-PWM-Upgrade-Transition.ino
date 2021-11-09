cb14e55e2c9c0ff9269122492b87a4d9cf464a00
Marcin Saj <marcin@nixietester.com>
2021-11-09 18:33:04 +0100

    Update 07I-ONE-Nixie-Clock---Classic-Nixie-Clock-PWM-Fade-In-Out-PWM-Upgrade-Transition.ino

--- examples/arduino-nano-33-iot/07I-ONE-Nixie-Clock---Classic-Nixie-Clock-PWM-Fade-In-Out-PWM-Upgrade-Transition.ino
+++ examples/arduino-nano-33-iot/07I-ONE-Nixie-Clock---Classic-Nixie-Clock-PWM-Fade-In-Out-PWM-Upgrade-Transition.ino
@@ -24,7 +24,7 @@
 // https://github.com/adafruit/Adafruit_NeoPixel
 // https://learn.adafruit.com/adafruit-neopixel-uberguide/arduino-library-use
 
-#include <RTClib.h>          
+#include <RTClib.h>
 // https://github.com/adafruit/RTClib
 
 // RTC library declaration
