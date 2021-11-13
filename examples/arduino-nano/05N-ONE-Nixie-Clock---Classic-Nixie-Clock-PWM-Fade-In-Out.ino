0d520fb58613a1fdd8bfdc24fdb59c3092bb31dc
Marcin Saj <marcin@nixietester.com>
2021-11-13 22:54:44 +0100

    Update 05N-ONE-Nixie-Clock---Classic-Nixie-Clock-PWM-Fade-In-Out.ino

--- examples/arduino-nano/05N-ONE-Nixie-Clock---Classic-Nixie-Clock-PWM-Fade-In-Out.ino
+++ examples/arduino-nano/05N-ONE-Nixie-Clock---Classic-Nixie-Clock-PWM-Fade-In-Out.ino
@@ -223,9 +223,6 @@ void setup()
   pinMode(DIN_PIN, OUTPUT);
   digitalWrite(DIN_PIN, LOW);
 
-  pinMode(LED_PIN, OUTPUT);
-  digitalWrite(LED_PIN, LOW);  
-
   pinMode(PWM_PIN, OUTPUT);
   digitalWrite(PWM_PIN, LOW);  
       
